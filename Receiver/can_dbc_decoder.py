73311ce1c79cc829b8bb4954f6f3e9807dd01cc0
owentmfoo <owentmfoo@gmail.com>
2024-08-15 22:57:59 +0100

    refactor dbc file paths to receiver_config.py

--- Receiver/can_dbc_decoder.py
+++ Receiver/can_dbc_decoder.py
@@ -1,21 +1,20 @@
-from pathlib import PurePath
+from typing import Union
 
 import cantools.database
+from Receiver.receiver_config import dbc_files
 
-dbc_folder = PurePath("../dbc/")
 database = cantools.database.Database()
-database.add_dbc_file(dbc_folder/"wavesculptor_22.dbc")
-database.add_dbc_file(dbc_folder/"MPPT.dbc")
-database.add_dbc_file(dbc_folder/"Telemetry.dbc")
-database.add_dbc_file(dbc_folder/"Orion.dbc")
+for dbc_file in dbc_files:
+    database.add_dbc_file(dbc_file)
 
 
-def decode_can_msg(canId, msgBytes) -> dict:
+def decode_can_msg(canId, msgBytes) -> dict[str, Union[int, float, str, bytes]]:
+    decode_msg: dict[str, Union[int, float, str, bytes]]
     decode_msg = database.decode_message(canId, msgBytes)
 
     # convert back to bytes for backwards compatibility
     if canId == 248:
-        decode_msg["GpsLat"] = decode_msg["GpsLat"].to_bytes(1,"little")
+        decode_msg["GpsLat"] = decode_msg["GpsLat"].to_bytes(1, "little")
     elif canId == 249:
-        decode_msg["GpsLon"] = decode_msg["GpsLon"].to_bytes(1,"little")
+        decode_msg["GpsLon"] = decode_msg["GpsLon"].to_bytes(1, "little")
     return decode_msg
--- Receiver/receiver_config.py
+++ Receiver/receiver_config.py
@@ -1,4 +1,6 @@
+from pathlib import PurePath
 from typing import NamedTuple
+
 
 # used by all live_calculation scripts
 class influxCredentials(NamedTuple):
@@ -9,7 +11,16 @@ class influxCredentials(NamedTuple):
     port: int = 8086
     enabled: bool = True
 
+
 ifCredentials = influxCredentials()
 
 # configFile: str = './CANConfig.xslx' #raspberrypi
-configFile: str = '../../CANTranslator/config/CANBusConfig.xlsm'  # testing with windows
+configFile: str = "../../CANTranslator/config/CANBusConfig.xlsm"  # testing with windows
+
+dbc_folder = PurePath("../dbc/")
+dbc_files = [
+    dbc_folder / "wavesculptor_22.dbc",
+    dbc_folder / "MPPT.dbc",
+    dbc_folder / "Telemetry.dbc",
+    dbc_folder / "Orion.dbc",
+]
